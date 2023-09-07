6dcb67dbdadfdbe53a19b64941e7591d44ae2685
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-09-07 15:06:55 +0900

    feat: Chatbotapp 리니어 레이아웃 버튼 intent

--- GptChatbotapp/app/src/main/java/com/example/gptchatbotapp/ui/home/HomeFragment.kt
+++ GptChatbotapp/app/src/main/java/com/example/gptchatbotapp/ui/home/HomeFragment.kt
@@ -1,41 +1,35 @@
 package com.example.gptchatbotapp.ui.home
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.example.gptchatbotapp.databinding.FragmentHomeBinding
+import com.example.gptchatbotapp.ChatActivity
+import com.example.gptchatbotapp.R
+import com.example.gptchatbotapp.ui.notifications.NotificationsFragment
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(R.layout.fragment_home) {
 
-private var _binding: FragmentHomeBinding? = null
-  // This property is only valid between onCreateView and
-  // onDestroyView.
-  private val binding get() = _binding!!
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-  override fun onCreateView(
-    inflater: LayoutInflater,
-    container: ViewGroup?,
-    savedInstanceState: Bundle?
-  ): View {
-    val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+        val button1 = view.findViewById<View>(R.id.button1)
+        val button2 = view.findViewById<View>(R.id.button2)
+        val button3 = view.findViewById<View>(R.id.button3)
 
-    _binding = FragmentHomeBinding.inflate(inflater, container, false)
-    val root: View = binding.root
+        button1.setOnClickListener {
+            val intent = Intent(requireActivity(), ChatActivity::class.java)
+            startActivity(intent)
+        }
 
-    val textView: TextView = binding.textHome
-    homeViewModel.text.observe(viewLifecycleOwner) {
-      textView.text = it
-    }
-    return root
-  }
+        button2.setOnClickListener {
+            val intent = Intent(requireActivity(), ChatActivity::class.java)
+            startActivity(intent)
+        }
 
-override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+        button3.setOnClickListener {
+            val intent = Intent(requireActivity(), ChatActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
